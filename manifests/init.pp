1a3bd45e2f9e21dba6cfd4f6fc1446dee9dc03ec
H.Y. Leung <hy.leung@gmail.com>
2012-08-21 14:31:48 -0700

    Enabled Riak Control
    - added self-signed SSL cert
    - enabled SSL on Riak
    - enable Riak Control
    - bound SSL to port 8069 on 0.0.0.0 (will need to set up port fowarding)

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,6 +15,17 @@ class riak {
 		group => 'puppet'
 	}
 
+	#install openssl
+	package {
+		"openssl":
+		ensure => installed
+	}
+	$pem_file = 'cert.pem'
+	$key_file = 'key.pem'
+	exec { "/usr/bin/openssl req -x509 -nodes -days 365 -newkey rsa:1024 -keyout $key_file -out $pem_file -subj  '/CN=com.foresee'":
+            cwd => "/etc/riak/",
+            creates => "/etc/riak/ssl/$pem_file"}
+
 	#install libssl
 	package { "libssl0.9.8":
 		ensure => installed
